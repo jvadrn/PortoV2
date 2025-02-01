0d2f62f3bb94a310caa5207cf1cd682ad5b009c7
Jevi Adriansyah <jeviadriansyah25@gmail.com>
2025-02-01 20:14:37 +0700

    update

--- next.config.js
+++ next.config.js
@@ -2,5 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
 }
-
+module.exports = {
+  output: 'export',
+  pageExtensions: ["md", "mdx", "js", "jsx", "ts", "tsx"],
+}
 module.exports = nextConfig
